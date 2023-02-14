3e0683e2c40c7061b54bb6f96e8f501ee83189e5
Udayan L.R <93900669+udayan-lr@users.noreply.github.com>
2023-02-14 13:07:46 +0000

    testing

--- MODULE1/10naturalnumbers.java
+++ MODULE1/10naturalnumbers.java
@@ -3,10 +3,10 @@ class natural10
 {
     public static void main(String[] args)
     {
-        int i;
-        for(i=1;i<=10;i++)
+        int a;
+        for(a=1;a<=10;a++)
         {
-            System.out.println(i);
+            System.out.println(a);
         } 
     }
 }

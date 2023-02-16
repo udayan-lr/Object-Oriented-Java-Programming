2f8e86c3ece858148afe0511071e490e27ffdf17
Udayan L.R <93900669+udayan-lr@users.noreply.github.com>
2023-02-16 08:17:08 +0000

    .

--- Other/squareofanumber.java
+++ Other/squareofanumber.java
@@ -5,6 +5,6 @@ class square
 		Scanner sc=new Scanner(System.in);
 		System.out.print("Enter the number that you want to find the square of: ");
 		float a=sc.nextFloat();
-		System.out.println("Square of the number = "+a*a);
+		System.out.println("Square of the number = "+ a*a);
 	}
 }
